70c202458d13e1e8d6f522f5cdb99808e7e8e5d6
zzf <zhznfei@yeah.net>
2017-10-25 23:38:23 +0800

    only multiply 0.9999

--- src/cn/edu/nju/cs/itrace4/demo/algo/outerVertex/process/UD_DataSubGraphWithBonusForLoneWithTrans.java
+++ src/cn/edu/nju/cs/itrace4/demo/algo/outerVertex/process/UD_DataSubGraphWithBonusForLoneWithTrans.java
@@ -213,7 +213,7 @@ public class UD_DataSubGraphWithBonusForLoneWithTrans implements CSTI{
 						double curValue = matrix.getScoreForLink(req, vertexName);
 						if(!vertexName.equals(represent)){
 							int graphSize = subGraph.getVertexList().size();
-							curValue = Math.min(maxScore, curValue+maxScore/(graphSize-1));
+							curValue = Math.min(maxScore*0.9999, curValue+maxScore/(graphSize-1));
 							maxScoreInThisSubGraph = Math.max(maxScoreInThisSubGraph, curValue);
 						}
 						matrix_ud.addLink(req, vertexName,curValue);
