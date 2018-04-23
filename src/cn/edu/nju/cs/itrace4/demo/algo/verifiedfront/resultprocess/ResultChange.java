ff5f191bb76129cea840363ae5dd212c48e5041c
zzf <tiaozhanzhe668@gmail.com>
2018-04-23 19:46:01 +0800

    place the verified link at front of list

---
+++ src/cn/edu/nju/cs/itrace4/demo/algo/verifiedfront/resultprocess/ResultChange.java
@@ -0,0 +1,19 @@
+package cn.edu.nju.cs.itrace4.demo.algo.verifiedfront.resultprocess;
+
+import java.util.Map;
+import java.util.Set;
+
+import cn.edu.nju.cs.itrace4.core.document.SingleLink;
+import cn.edu.nju.cs.itrace4.core.metrics.Result;
+
+public class ResultChange {
+	public static void modifyResult(Result result,Map<String,Set<String>> verifiedLinkList) {
+		for(SingleLink singleLink:result.matrix.allLinks()) {
+			String req = singleLink.getSourceArtifactId();
+			String className = singleLink.getTargetArtifactId();
+			if(verifiedLinkList.containsKey(req) && verifiedLinkList.get(req).contains(className)) {
+				result.matrix.setScoreForLink(req, className, 1.0);
+			}
+		}
+	}
+}
