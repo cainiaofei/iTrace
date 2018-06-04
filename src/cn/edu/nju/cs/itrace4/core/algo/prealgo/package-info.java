4beba110494957a53a09def45370c1c2d872d779
zzf <tiaozhanzhe668@gmail.com>
2018-06-04 17:09:55 +0800

    Optimize the directory structure so that they have the same level of abstraction

---
+++ src/cn/edu/nju/cs/itrace4/core/algo/prealgo/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * @author zzf
+ * @date 2018.6.4
+ * @description this package contains class write by niejia. Before, these large numbers of 
+ * classes were placed in *core.algo*.  It seemed so unsatisfied that I move them in this new 
+ * package called *core.algo.prealgo*. 
+ */
+package cn.edu.nju.cs.itrace4.core.algo.prealgo;
