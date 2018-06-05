bd66354b0b74527028459252fd00d04b33ded191
zzf <tiaozhanzhe668@gmail.com>
2018-06-05 20:01:31 +0800

    new branch

---
+++ src/cn/edu/nju/cs/itrace4/demo/explore/closeness/Edge.java
@@ -0,0 +1,37 @@
+package cn.edu.nju.cs.itrace4.demo.explore.closeness;
+
+public class Edge {
+	private int vertexA;
+	private int vertexB;
+	private double closeness;
+	
+	public Edge(int vertexA,int vertexB,double closeness) {
+		this.vertexA = vertexA;
+		this.vertexB = vertexB;
+		this.closeness = closeness;
+	}
+	
+	public int getVertexA() {
+		return vertexA;
+	}
+	
+	public void setVertexA(int vertexA) {
+		this.vertexA = vertexA;
+	}
+	
+	public int getVertexB() {
+		return vertexB;
+	}
+	
+	public void setVertexB(int vertexB) {
+		this.vertexB = vertexB;
+	}
+	
+	public double getCloseness() {
+		return closeness;
+	}
+	
+	public void setCloseness(double closeness) {
+		this.closeness = closeness;
+	}
+}
