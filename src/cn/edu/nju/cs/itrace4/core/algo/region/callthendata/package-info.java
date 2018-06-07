0ce293313e2a25e84276aa555f87f4eaeb731d74
zzf <tiaozhanzhe668@gmail.com>
2018-06-07 10:31:29 +0800

    place algorithm which optimize IR List through call graph then data graph in ..core.algo.region.callthendata.

---
+++ src/cn/edu/nju/cs/itrace4/core/algo/region/callthendata/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * @author zzf
+ * @date 2018.6.6
+ * @description for the IR list, firstly optimize the IR list through the call dependency graph.
+ * 	 next, optimize the after-call-optimize IR list.
+ */
+package cn.edu.nju.cs.itrace4.core.algo.region.callthendata;
